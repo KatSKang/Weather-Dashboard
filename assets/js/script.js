1a25d73490f97599a3fede703ce04013cdb2df5c
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-08-14 23:54:29 -0400

    updated html,css, added 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,105 @@
-var searchInput = document.getElementById('city-name');
+var searchInput = document.getElementById('city-search');
 var submitBtn = document.querySelector('.btn');
-var temp1 = document.getElementById('temp1');
-
-
+var cardDeck = document.querySelector('.card-deck');
 
 var listBtn = ''; // need to insert into HTML using JS
 var apiKey = '&appid=060124fc13f557f4671002d9efa9f884';
 
+//gets weather for current day
 function getDailyWeather () {
     var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchInput.value + apiKey;
 
-    fetch(geoUrl)
+    fetch(geoUrl) //get lat and long for city entered in search
         .then(function (response) {
+        if(!response.ok){
+            throw response.json();
+        } else {
             return response.json();
+        }
         })
-        .then(function (data) {
+        .then(function (data) { //use the lat and long as param for weather call
             var lat = data[0].lat;
             var lon = data[0].lon;
             console.log('The latitude is ' + data[0].lat + ', the longitude is ' + data[0].lon);
-            var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&' + 'lon=' + lon + apiKey;
-            return fetch(weatherUrl)
+            var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&' + 'lon=' + lon + apiKey + '&units=imperial';
+            return fetch(weatherUrl) // get weather info
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(weatherData) { //insert weather data into html
+            console.log(weatherData);
+            $('#cityname').text(weatherData.name);
+            $('#date').text(`(${moment().format("l")})`); 
+            $('#icon').attr('src',`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`);
+            $('#temp1').text(weatherData.main.temp);
+            $('#hum1').text(weatherData.main.humidity);
+            $('#wind1').text(weatherData.wind.speed);
+        })
+};
+
+//gets UV index for current day & next 5 days info
+function getUvAndFive() {
+    var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + searchInput.value + apiKey;
+
+    fetch(geoUrl) //get lat and long for city entered in search
+        .then(function (response) {
+        if(!response.ok){
+            throw response.json();
+        } else {
+            return response.json();
+        }
+        })
+        .then(function (data) { //use the lat and long as param for weather call
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            var oneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&' + 'lon=' + lon + apiKey+ '&units=imperial';
+            return fetch(oneCallUrl) // get UV index + next 5 days
         })
         .then(function(response) {
             return response.json();
         })
         .then(function(data) {
             console.log(data);
-            console.log('The temp is ' + data.main.temp);
+            var uvIndex = data.current.uvi;
+            if(uvIndex <= 2.0) {
+                $('#uv1').text(uvIndex);
+                 $('#uv1').addClass('bg-success');    
+            } else if (uvIndex > 2 && uvIndex < 6) {
+                $('#uv1').text(uvIndex);
+                $('#uv1').addClass('bg-warning');
+            } else if (uvIndex >= 6) {
+                $('#uv1').text(uvIndex);
+                $('#uv1').addClass('bg-danger');
+            }
 
+            var cardHtml = '';
+            for(var i = 1; i < 6; i++) {
+                var icon = data.daily[i].weather[0].icon;
+                var date = moment.unix(data.daily[i].dt).format("MM-DD-YYYY");
+                cardHtml += `
+                <div class="card" id="small-cards">
+                <h5 class="card-title">${date}</h5>
+                <img src="http://openweathermap.org/img/wn/${icon}@2x.png" class="card-img-top" alt="Weather description">
+                    <div class="card-body">
+                        <p class="card-text">Temp: ${data.daily[i].temp.day}°F</p>
+                        <p class="card-text">Humidty: ${data.daily[i].humidity}%</p>
+                        <p class="card-text">Wind: ${data.daily[i].wind_speed} mph</p>
+                    </div>
+                </div>`;
 
-        })
-        .catch(function(error) {
-            console.log('Request failed', error);
+                cardDeck.innerHTML = cardHtml;
+
+            }
         });
-};
+    };
+
+
+ 
+    
+
+
+
 
 
 
@@ -49,4 +115,4 @@ function getDailyWeather () {
 
 // EVENT LISTENSERS 
 submitBtn.addEventListener('click',getDailyWeather);
-
+submitBtn.addEventListener('click',getUvAndFive);
